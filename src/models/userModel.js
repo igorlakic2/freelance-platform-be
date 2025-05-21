80543204301681ae472b668c8d2d18278d13de94
Igor <igor.lakic2@gmail.com>
2025-05-21 23:25:25 +0200

    login and sign up services

---
+++ src/models/userModel.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const usersSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ["FREELANCER", "CLIENT"],
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("User", usersSchema);
